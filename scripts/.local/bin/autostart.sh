222662607a823540034c9ad7ce33fb3e4dca92e6
Sviatoslav Dudik <slavdudik303@gmail.com>
2020-09-03 17:17:07 +0200

    Add actions before sleep
    Before going to sleep the 'presleep' script will be executed. It will
    lock the GPG keyring and the screen.

--- scripts/.local/bin/autostart.sh
+++ scripts/.local/bin/autostart.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+[ -x /usr/local/bin/slstatus ] && /usr/local/bin/slstatus&
+[ -x $HOME/.local/bin/presleep ] && xss-lock $HOME/.local/bin/presleep&
+[ -x /usr/bin/xbindkeys ] && /usr/bin/xbindkeys -f "$XDG_CONFIG_HOME"/xbindkeys/config
 xmodmap $XDG_CONFIG_HOME/X11/keymaps
+/usr/local/bin/trackpoint
